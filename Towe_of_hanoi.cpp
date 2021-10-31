0bcc2a5ca4c9bf2bfe2012dfa9b67ff363d4a19f
harvinder300 <harvinderkumar.kumar003@gmail.com>
2021-10-31 18:00:52 +0530

    Added Tower_of_hanoi program

---
+++ Towe_of_hanoi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+void TOH(int n, char a, char b, char c)
+{
+    if (n == 1)
+    {
+        cout << "Move 1 from " << a << " to " << c << endl;
+        return;
+    }
+    TOH(n - 1, a, c, b);
+    cout << "Move " << n << " from " << a << " to " << c << endl;
+    TOH(n - 1, b, a, c);
+}
+int main()
+{
+    char a, b, c;
+    cin >> a >> b >> c;
+    int n;
+    cin >> n;
+    TOH(n, a, b, c);
+    return 0;
+}
